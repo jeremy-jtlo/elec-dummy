fc439ed33644b6a6999c54b44bb5f51ba2726b00
Jeremy <y2kfirzen@gmail.com>
2017-12-10 20:39:12 -0800

    Adding the first few main components

---
+++ src/components/panels/leftPanel.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class LeftPanel extends Component {
+    render() {
+        /* Skeleton for a left control panel */
+        return(
+            <div className="leftPanel">
+                <h1>This is the Left Panel</h1>
+            </div>
+        );
+    }
+}
---
+++ src/components/panels/mainPanel.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Main extends Component {
+    render() {
+        /* Skeleton for a main content panel */
+        return(
+            <div className="mainPanel">
+                <h1>This is the Main Panel</h1>
+            </div>
+        );
+    }
+}
