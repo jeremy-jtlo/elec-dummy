be9b855038fd9187af7b040fec7559064e4d2909
Jeremy <y2kfirzen@gmail.com>
2018-01-14 14:57:09 -0800

    Enabled independent column scroll, but not very neatly

--- src/components/panels/queueView.js
+++ src/components/panels/queueView.js
@@ -4,10 +4,10 @@ import {List, ListItem} from 'material-ui/List';
 export default class QueueView extends Component {
     render() {
         /* TODO: Connect array to backend song list */
-        var dummyArray = Array(15).fill(<ListItem>Dummy Song Data</ListItem>);
+        var dummyArray = Array(30).fill(<ListItem>Dummy Song Data</ListItem>);
         
         return(
-            <List className="col-3of12">
+            <List className="playQueue col-3of12">
                 {dummyArray}
             </List>
         );
